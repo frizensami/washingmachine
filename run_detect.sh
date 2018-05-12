8b0a91bf81523910de149cc3701e5c50c3f1f4d0
Sriram Sami <frizensami@gmail.com>
2018-05-12 22:50:47 +0800

    Ignore logs folder, write to logs folder

--- run_detect.sh
+++ run_detect.sh
@@ -1,1 +1,3 @@
-python -u /home/pi/Documents/washingmachine/detect.py | tee /home/pi/Documents/washingmachine/log.txt 
+mkdir log
+NOW=$(date +"%Y-%m-%d-%H-%M-%S")
+python -u /home/pi/Documents/washingmachine/detect.py | tee /home/pi/Documents/washingmachine/log/log-$NOW.txt 
