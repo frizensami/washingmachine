3d8b61c728fb99ef40cdb619b09db363d7ff975d
Sriram Sami <frizensami@gmail.com>
2018-05-12 21:53:59 +0800

    Adjust config file (pin assignments)

--- config.py
+++ config.py
@@ -26,16 +26,16 @@ BOUNCETIME = 50
 FLOOR_NUMBER = "9"
 
 WASHERS = [
-        {	"pin":18, "name":"WASHER_A", "machinetype": WASHER, "paymenttype":PAY_EZLINK	},
-	{	"pin":23, "name":"WASHER_B", "machinetype": WASHER, "paymenttype":PAY_EZLINK	},
-	{	"pin":24, "name":"WASHER_C", "machinetype": WASHER, "paymenttype":PAY_EZLINK	},
-	{	"pin":25, "name":"WASHER_D", "machinetype": WASHER, "paymenttype":PAY_COIN	},
-	{	"pin":8,  "name":"WASHER_E", "machinetype": WASHER, "paymenttype":PAY_COIN	},
+        {	"pin":21, "name":"WASHER A", "machinetype": WASHER, "paymenttype":PAY_EZLINK	},
+	{	"pin":20, "name":"WASHER B", "machinetype": WASHER, "paymenttype":PAY_EZLINK	},
+	{	"pin":16, "name":"WASHER C", "machinetype": WASHER, "paymenttype":PAY_EZLINK	},
+	{	"pin":12, "name":"WASHER D", "machinetype": WASHER, "paymenttype":PAY_COIN	},
+	{	"pin":7,  "name":"WASHER E", "machinetype": WASHER, "paymenttype":PAY_COIN	},
 ]
 
 DRYERS = [
-        {	"pin":18, "name":"DRYER_A", "machinetype": WASHER, "paymenttype": PAY_EZLINK	},
-	{	"pin":23, "name":"DRYER_B", "machinetype": WASHER, "paymenttype": PAY_EZLINK	},
-	{	"pin":24, "name":"DRYER_C", "machinetype": WASHER, "paymenttype": PAY_COIN		},
-	{	"pin":25, "name":"DRYER_D", "machinetype": WASHER, "paymenttype": PAY_COIN		},
+        {	"pin":25, "name":"DRYER A", "machinetype": DRYER, "paymenttype": PAY_EZLINK	},
+	{	"pin":24, "name":"DRYER B", "machinetype": DRYER, "paymenttype": PAY_EZLINK	},
+	{	"pin":23, "name":"DRYER C", "machinetype": DRYER, "paymenttype": PAY_COIN	},
+	{	"pin":18, "name":"DRYER D", "machinetype": DRYER, "paymenttype": PAY_COIN	},
 ]
